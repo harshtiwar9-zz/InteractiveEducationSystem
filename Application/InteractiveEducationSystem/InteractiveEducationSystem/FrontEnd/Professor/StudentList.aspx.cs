4185ed30bfa07300d4fb7af5f042e21a5f32c55e
Harsh Tiwar <harsh.tiwar9@gmail.com>
2018-07-13 16:25:36 -0400

    With MasterPage

--- Application/InteractiveEducationSystem/InteractiveEducationSystem/FrontEnd/Professor/StudentList.aspx.cs
+++ Application/InteractiveEducationSystem/InteractiveEducationSystem/FrontEnd/Professor/StudentList.aspx.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
-using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -14,24 +11,6 @@ namespace InteractiveEducationSystem.FrontEnd.Professor
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            String connectionString = WebConfigurationManager.ConnectionStrings["IES"].ConnectionString;
-            SqlConnection con = new SqlConnection(connectionString);
-            SqlCommand com = new SqlCommand("SELECT *  from Student", con);
-
-            try
-            {
-                con.Open();
-                SqlDataAdapter adp = new SqlDataAdapter(com);
-                DataSet ds = new DataSet();
-                adp.Fill(ds);
-                StudentList1.DataSource = ds;
-                StudentList1.DataBind();
-            }
-            catch
-            {
-                con.Close();
-            }
-
 
         }
     }
