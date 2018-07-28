2ab81034b33bc29594daa1a2d682c09a848644d9
Priyank926 <priyank926@gmail.com>
2018-07-28 15:21:47 -0400

    Professor now can give his comments / Feedback to Quiz students marked.

--- Application/InteractiveEducationSystem/InteractiveEducationSystem/FrontEnd/Professor/ProfessorCourses.aspx.designer.cs
+++ Application/InteractiveEducationSystem/InteractiveEducationSystem/FrontEnd/Professor/ProfessorCourses.aspx.designer.cs
@@ -65,5 +65,14 @@ namespace InteractiveEducationSystem.FrontEnd.Professor {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.SqlDataSource IES;
+        
+        /// <summary>
+        /// SqlDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
     }
 }
