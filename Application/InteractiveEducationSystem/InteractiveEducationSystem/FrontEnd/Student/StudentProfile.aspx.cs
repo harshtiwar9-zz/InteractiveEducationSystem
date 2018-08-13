e391c066e7afd2a314fa0fb2d9b3389eac06bff2
Swapnika Patel <swapnikapatel2016@gmail.com>
2018-08-13 14:49:50 -0400

    StudentProfile

---
+++ Application/InteractiveEducationSystem/InteractiveEducationSystem/FrontEnd/Student/StudentProfile.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace InteractiveEducationSystem.FrontEnd.Student
+{
+    public partial class StudentProfile : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void UpdateProfile(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
