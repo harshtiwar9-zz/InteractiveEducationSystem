ea2f20808a048c3f55e8984c752df77174c0e296
gohelrajan <39545736+gohelrajan@users.noreply.github.com>
2018-08-13 15:13:27 -0400

    sending user id & role in session

--- Application/InteractiveEducationSystem/InteractiveEducationSystem/Login.aspx.cs
+++ Application/InteractiveEducationSystem/InteractiveEducationSystem/Login.aspx.cs
@@ -11,18 +11,24 @@ using System.Web.UI.WebControls;
 
 namespace InteractiveEducationSystem
 {
+    
     public partial class Login : System.Web.UI.Page
     {
         string role;
         string result;
+        string userName;
+        string password;
+        string connectionString = ConfigurationManager.ConnectionStrings["IES"].ConnectionString;
+        SqlConnection mycon;
+        SqlCommand cmd;
         protected void Page_Load(object sender, EventArgs e)
         {
             
         }
         protected void loginbutton_Click(object sender, EventArgs e)
         {
-            string userName = Loginbox.Text;
-            string password = Passwordbox.Text;
+            userName = Loginbox.Text;
+            password = Passwordbox.Text;
 
             result = UserLogin(userName, password);
 
@@ -69,23 +75,39 @@ namespace InteractiveEducationSystem
 
         public void Page_re(string role)
         {
+            
+            mycon = new SqlConnection(connectionString);
+
             if (role != "no_user")
             {
                 if(role.Equals("s"))
                 {
+                    cmd = new SqlCommand("SELECT First_name from Student WHERE Id_FK = @UserName", mycon);
+
+                    Session["username"] =userName ;
+                    Session["role"] = role;
+
                     Server.Transfer("FrontEnd/Student/StudentHome.aspx");
-                    Session["username"] = Loginbox.Text;
                 }
                 else if(role.Equals("p"))
                 {
+                   
+                    Session["username"] = userName;
+                    Session["role"] = role;
                     Server.Transfer("FrontEnd/Professor/ProfessorHome.aspx");
                 }
                 else if(role.Equals("c"))
                 {
+                   
+                    Session["username"] = userName;
+                    Session["role"] = role;
                     Server.Transfer("CoursecoHome.aspx");
                 }
                 else if(role.Equals("a"))
                 {
+                  
+                    Session["username"] = userName;
+                    Session["role"] = role;
                     Server.Transfer("FrontEnd/Administrator/AdminHome.aspx");
                 }
                
